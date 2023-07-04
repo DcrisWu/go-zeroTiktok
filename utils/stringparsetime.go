fa5b6f4f66506bbb7317a52acf43aa18d03d0f22
DcrisWu <dcris@foxmail.com>
2023-07-04 18:20:38 +0800

    新增string转time功能

---
+++ utils/stringparsetime.go
@@ -0,0 +1,22 @@
+package utils
+
+import "time"
+
+var timeTemplates = []string{
+	"2006-01-02 15:04:05", //常规类型
+	"2006/01/02 15:04:05",
+	"2006-01-02",
+	"2006/01/02",
+	"15:04:05",
+}
+
+/* 时间格式字符串转换 */
+func TimeStringToGoTime(tm string) time.Time {
+	for i := range timeTemplates {
+		t, err := time.ParseInLocation(timeTemplates[i], tm, time.Local)
+		if nil == err && !t.IsZero() {
+			return t
+		}
+	}
+	return time.Time{}
+}
